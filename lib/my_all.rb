8570fad8554912340e11b769d5879c043f35ffe6
Lexi Mutia <muse.malize@gmail.com>
2019-12-14 17:49:08 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,14 @@
-require 'pry'
-
-def my_all?(collection)
-
-end
+def my_collect(array)
+  
+  collection = []
+  
+  i = 0 
+  while i < array.length 
+    collection << yield(array[i])
+    
+    i += 1
+  end 
+  
+  collection
+  
+end 
